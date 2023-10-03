255e3e1686faddba7932250127b23fef329b3b3d
Corey Atkinson <catkinson2025@gmail.com>
2023-10-03 09:27:34 -0500

    Initial Commit

---
+++ ClickerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClickerApp
+//
+//  Created by COREY ATKINSON on 10/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
