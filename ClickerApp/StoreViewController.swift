33bcc5bcb6afae1cf3be80ef981307080dc18a9c
Corey Atkinson <catkinson2025@gmail.com>
2023-10-05 09:30:24 -0500

    yay

--- ClickerApp/StoreViewController.swift
+++ ClickerApp/StoreViewController.swift
@@ -8,22 +8,89 @@
 import UIKit
 
 class StoreViewController: UIViewController {
-
+    var points = 0
+    var delegate : ViewController!
+    var two: Bool
+    var five: Bool
+    var twenty: Bool
+    
+    @IBOutlet weak var twentyOutlet: UIButton!
+    @IBOutlet weak var fiveOutlet: UIButton!
+    @IBOutlet weak var twoOutlet: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+if two
+        {
+            twoOutlet.isEnabled = true
+        }
+        else
+        {
+            twoOutlet.isEnabled = false
+        }
+        if five
+                {
+                    fiveOutlet.isEnabled = true
+                }
+                else
+                {
+                    fiveOutlet.isEnabled = false
+                }
+        if twenty
+                {
+                    twentyOutlet.isEnabled = true
+                }
+                else
+                {
+                    twentyOutlet.isEnabled = false
+                }
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func Action(_ sender: Any) {
+        if delegate.total < 10
+        {
+            print("error")
+        }
+        else
+        {
+            delegate.total = delegate.total - 10
+            delegate.multiply = 2
+            twoOutlet.isEnabled = false
+            delegate.labelOutlet.text = "\(delegate.total)"
+            two = false
+        }
     }
-    */
-
+    
+    @IBAction func fiveAction(_ sender: UIButton) {
+        if delegate.total < 200
+        {
+            print("error")
+        }
+        else
+        {
+            delegate.total = delegate.total - 200
+            delegate.multiply = 5
+            fiveOutlet.isEnabled = false
+            delegate.labelOutlet.text = "\(delegate.total)"
+            five = false
+        }
+        
+    }
+    
+    @IBAction func twentyAction(_ sender: UIButton) {
+        if delegate.total < 1000
+        {
+            print("error")
+        }
+        else
+        {
+            delegate.total = delegate.total - 1000
+            delegate.multiply = 20
+            twentyOutlet.isEnabled = false
+            delegate.labelOutlet.text = "\(delegate.total)"
+            twenty = false
+        }
+        
+    }
+    
 }
--- ClickerApp/ViewController.swift
+++ ClickerApp/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var total = 0
+    var multiply = 1
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var labelOutlet: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickAction(_ sender: UIButton) {
+        total = total + (1 * multiply)
+        print(total)
+            labelOutlet.text = "\(total)"
+    }
+    
+    @IBAction func toStoreAction(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "StoreSegue", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let nvc = segue.destination as! StoreViewController
+        nvc.points = total
+        nvc.delegate = self
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
